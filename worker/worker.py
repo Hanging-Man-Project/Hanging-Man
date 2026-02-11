6d86c195f51c76ceb2a4bd589158358fd02f9a71
Edouard Lambert <edouard.lambert@student.junia.com>
2026-02-11 16:14:38 +0100

    random word function in worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,11 @@
+import random
 import time
 
+def get_random_word():
+    with open("words_dictionary.txt", "r") as dict_file:
+        words = dict_file.read().splitlines()
+    return random.choice(words)
+
 print("Worker started")
-time.sleep(3)
-print("Task completed successfully")
+print(get_random_word())
 print("Worker exiting cleanly")
