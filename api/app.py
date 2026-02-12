10c6b918cc798fe0e754b221e9037647781ca6ae
Henri OMS <henri.oms@student.junia.com>
2026-02-12 16:02:40 +0100

    API : route start game
    créationd e la route start qui récupère le mot du worker

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import httpx
+import uuid
 
 app = FastAPI()
+
+WORKER_URL = "http://worker:8000/random-word"
 
 @app.get("/")
 def root():
@@ -9,3 +13,21 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/start")
+async def start_game(request: StartRequest):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(WORKER_URL)
+            response.raise_for_status()
+            data = response.json()
+            word = data.get("word", "")
+
+            masked_word = "_" * len(word)
+            
+            return {
+                "game_id": str(uuid.uuid4())[:6], 
+                "letters": masked_word
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Erreur Worker: {str(e)}")
